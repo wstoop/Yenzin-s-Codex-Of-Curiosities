5f3b853a225065b367205583438a6a19786263a7
wstoop <warre.stoop@gmail.com>
2024-09-26 14:51:04 +0200

    Quartz sync: Sep 26, 2024, 2:51 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "🪴 Quartz 4.0",
+    pageTitle: "Yenzin's Codex of Curiosities",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
